7bbf76142f439878dc7fa7bde7d4307fc764d86e
gaelWumba <gael.wumbabuatu@gmail.com>
2024-07-19 11:30:41 +0200

    Initial Commit

---
+++ MyFacetime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFacetime
+//
+//  Created by gael on 19/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
