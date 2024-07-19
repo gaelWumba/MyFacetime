12398ee8b787928d6965bdeea10fdf17979fb41e
gaelWumba <gael.wumbabuatu@gmail.com>
2024-07-19 15:31:41 +0200

    MyFacetime initialisation

---
+++ MyFacetime/Manager/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  MyFacetime
+//
+//  Created by gael on 19/07/2024.
+//
+
+import Foundation
---
+++ MyFacetime/Manager/CallManager.swift
@@ -0,0 +1,8 @@
+//
+//  CallManager.swift
+//  MyFacetime
+//
+//  Created by gael on 19/07/2024.
+//
+
+import Foundation
