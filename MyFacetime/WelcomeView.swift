ec83ea0fd96c41e46dcc4590b91248a5aa42087e
gaelWumba <gael.wumbabuatu@gmail.com>
2024-07-22 13:49:28 +0200

    MyFacetime final project

---
+++ MyFacetime/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  MyFacetime
+//
+//  Created by gael on 22/07/2024.
+//
+
+import UIKit
+
+class WelcomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
